b74177d1b4bffcd984212b5fc687fe8db868a9bb
18176962906 <1761824166@qq.com>
2018-11-21 12:12:38 +0800

    Summary

--- 购物车/js/shoppingCart.js
+++ 购物车/js/shoppingCart.js
@@ -1,12 +1,3 @@
-localStorage.setItem("key", "value");//存储变量名为key，值为value的变量 
-
-localStorage.getItem("key");//获取存储的变量key的值
-
-localStorage.removeItem("key")//删除变量名为key的存储变量
-
-
-
-
 //商品类
 class Product {
     constructor(id, title, price, imgSrc) {
@@ -27,7 +18,7 @@ class Order {
         this.price = product.price;
         this.imgSrc = product.imgSrc;
         this.qty = qty;
-        this.selectStatus = selectStatus
+        this.selectStatus = selectStatus;
     }
 }
 
@@ -38,7 +29,7 @@ class CartData {
     constructor() {
         this.orderList = new Array();
         this.units = 0;
-        this.totalQt = 0;
+        this.totalQty = 0;
         this.totalAmount = 0;
         this.units = 0;
 
@@ -47,22 +38,71 @@ class CartData {
 
 //购物车操作类
 class ShoppingCart {
-    //从本地存储中获取购物车数据
+
+    // 将购物车数据写入本地存储中
+    setDataToLocalSatorge(cartData) {
+        //清除原有存储写入新列表
+        localStorage.removeItem('lzzyCart');
+        //写入本地存储
+        localStorage.setItem('lzzyCart', JSON.stringify(cartData));
+    }
+
+    // 从本地存储中获取购物车数据
     getDataFromLocalStorage() {
-        return localStorage.getItem('lzzyCart');
-        //判断购物车是否为空
-        if(lzzyCart == nill || lzzyCart == ''){
-            return new CartData;
+        let lzzyCart = localStorage.getItem('lzzyCart');
+        // 判断购物车是否为空
+        if (lzzyCart == null || lzzyCart == '') {
+            return new CartData();
         }
         else {
             return JSON.parse(lzzyCart);
         }
     }
-    //将购物车数据写入本地
-    setDataToLocalSatorge(CartData) {
-        var CartDataString = JSON.stringify(CartData);
-        localStorage.setItem('test', CartDataString);
 
+
+    addToCart(order) {
+        //从本地存储中获取购物车的数据c
+        let cartData = this.getDataFromLocalStorage();
+        //获取购物车json数据中的订单列表
+        let orderList = cartData.orderList;
+        //设置标志位判断是否为购物车新商品，默认为是新商品
+        let isNewProduct = true;
+        //遍历订单列表，判断新加入商品是否在购物车中
+
+
+        for (let i in orderList) {
+            if (order.id == orderList[i].id) {
+                //若新加入订单商品已经在购物车中，则变更订单列表中对应的商品数量，且变更新商品标志位
+                orderList[i].qty += order.qty;
+                isNewProduct = false;
+                break;
+            }
+        }
+        //如果是新商品
+        if (isNewProduct) {
+            //购物车总样本+1
+            cartData.units++;
+            //导入新商品置入购物车
+            orderList.push(order);
+        }
+        //修改购物车总金额及商品总数量
+        cartData.totalAmount += order.qty * order.price;
+        cartData.totalQty += order.qty;
+        //写入localStorage
+        this.setDataToLocalSatorge(cartData);
+
+    }
+    getSelectedQty(){
+        let statusQty = 0;
+        let cartData = this.getDataFromLocalStorage();
+        for(let i = 0; i <cartData.orderList.length; i++){
+            if(cartData.orderList[i].selectStatus){
+                statusQty+=cartData.orderList[i].qty;
+            }
+        }
+        return statusQty;
     }
 
 }
+
+
