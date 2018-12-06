32f11de82b21f2251def7bb9fd4b3507dd8c50c4
18176962906 <1761824166@qq.com>
2018-12-06 18:11:26 +0800

    Summary

---
+++ 新建文件夹/WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "hello world! MVC!";
+        }
+        public Customer GetCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerNmae = "abc";
+            ct.Address = "def";
+            return ct;
+        }
+    }
+}
